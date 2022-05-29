253eae16228bf899c6da1ffeed542f0c3b66e530
Fakhre Alam <fakhrealam9537@gmail.com>
2022-05-29 14:07:59 +0530

    created brownie FundMe contract and tested it.

---
+++ scripts/deploy.py
@@ -0,0 +1,30 @@
+from brownie import config, FundMe, MockV3Aggregator, network
+from scripts.helpful_script import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+
+
+# if we are on a persistent network rinkeby, use the associated address, otherwise deploy mocks
+
+def deploy_fund_me():
+     account = get_account()
+     
+     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+          price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+     else:
+          # deploying our own version of pricefeed address on local ganache
+          deploy_mocks()
+          price_feed_address = MockV3Aggregator[-1].address
+     
+     # pass the price feed address to our FundMe contract
+     fund_me = FundMe.deploy(
+          price_feed_address,
+          {'from': account}, 
+          publish_source = config['networks'][network.show_active()].get('verify'),
+     )
+     print("Deployed at:", fund_me.address)
+     return fund_me
+
+    
+
+def main():
+    print("Deploying...")
+    deploy_fund_me()
